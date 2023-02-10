8aed80cfdeace719bd1e9b94bd87759609132260
AlexTAHI <62557152+alexxtahi@users.noreply.github.com>
2023-02-10 15:37:22 +0100

    Carousel integration in ActusView

--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -1,17 +1,15 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 import styles from '../../styles.css';
-import PoppinsText from "./PoppinsText";
 
 export default function CarouselItem(props: any) {
+    // Component rendering
     return (
         <TouchableOpacity style={styles.carouselItem}>
-            <PoppinsText
-                variant={"Bold"}
-                style={{ fontSize: 50, color: 'black' }}
-                content={props.content}
+            <Image
+                resizeMode={"cover"}
+                style={styles.aLaUneImg}
+                source={{ uri: props.img }}
             />
-
-
         </TouchableOpacity>
     );
 }
--- src/components/SnapCarousel.tsx
+++ src/components/SnapCarousel.tsx
@@ -1,28 +1,48 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
-import Carousel from "react-native-snap-carousel";
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 import CarouselItem from "./CarouselItem";
 import styles from '../../styles.css';
+import aLaUneJson from '../data/a_la_une.json';
 
 export default function SnapCarousel() {
-    // Properties
-    const datas: any[] = [1, 2, 3, 4];
-    const carouselRef: any = useRef();
+    // Data loading
+    const aLaUne: any[] = [];
+    aLaUneJson.forEach((img) => {
+        aLaUne.push(img);
+    });
     // Item building
     const renderItem = ({ item }: { item: any }) => (
-        <CarouselItem content={item} />
+        <CarouselItem img={item.src} />
     );
+    // Properties
+    const [activeSlide, setActiveSlide] = useState(0);
+    const carouselRef: any = useRef();
     // Component rendering
-    return (<View style={styles.carouselContainer}>
-        <Carousel
-            ref={carouselRef}
-            data={datas}
-            renderItem={renderItem}
-            sliderWidth={Dimensions.get('window').width}
-            itemWidth={Dimensions.get('window').width * 0.85}
-            slideStyle={styles.carouselSlide}
-        />
-    </View>
+    return (
+        <View style={styles.carouselContainer}>
+            <Carousel
+                ref={carouselRef}
+                data={aLaUne}
+                renderItem={renderItem}
+                sliderWidth={Dimensions.get('window').width}
+                itemWidth={Dimensions.get('window').width * 0.85}
+                slideStyle={styles.carouselSlide}
+                inactiveSlideOpacity={0.4}
+                onSnapToItem={(index) => setActiveSlide(index)}
+            />
+            {/* TODO: Finish this */}
+            <Pagination
+                dotsLength={aLaUne.length}
+                activeDotIndex={activeSlide}
+                containerStyle={styles.carouselPagination}
+                dotStyle={styles.carouselDot}
+                inactiveDotStyle={{ /* Define styles for inactive dots here*/ }}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.6}
+                animatedDuration={100}
+            />
+        </View>
     );
 }
 
